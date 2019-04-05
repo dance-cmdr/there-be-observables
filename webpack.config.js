b602e477d44a87824313c9954683ad435189099c
Alexandros Spyropoulos <1997886+dance-cmdr@users.noreply.github.com>
2019-04-05 19:49:51 +0100

    Ephemeral deployments to s3

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,31 @@
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.ts",
-    mode: 'development',
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  entry: './src/index.ts',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'inline-source-map',
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-    plugins: [
-        new CopyPlugin([
-          {
-            from: 'src/index.html',
-            to: 'index.html',
-            toType: 'file',
-          },
-        ]),
-      ],
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }],
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: 'src/index.html',
+        to: 'index.html',
+        toType: 'file',
+      },
+    ]),
+  ],
 };
