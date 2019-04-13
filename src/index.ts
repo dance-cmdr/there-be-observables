822734217bbdfd1d71d38c0234edb109047be996
Alexandros Spyropoulos <1997886+dance-cmdr@users.noreply.github.com>
2019-04-13 01:44:29 +0100

    Animate thrust (#16)
    * Rocket thrusts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
 import { GameScene } from './Game/Scene/GameScene';
 import { rocketFactory } from './Rocket/RocketModel';
 import { interval, fromEvent, animationFrameScheduler } from 'rxjs';
+import { playerInterface } from './InputInterface/Keyboard';
+import { spaceCraftFactory } from './SpaceCraft/SpaceCraft';
+import { Vector3 } from 'three';
 
 const ROCKET_SIZE = 1;
 
-const animationClock$ = interval(1000 / 60, animationFrameScheduler);
+const gameClock$ = interval(1, animationFrameScheduler);
 const windowSize$ = fromEvent(window, 'resize');
 
-const gameScene = new GameScene(animationClock$, windowSize$);
+const gameScene = new GameScene(gameClock$, windowSize$);
 gameScene.mount(document.getElementById('game'));
 
 const rocket = rocketFactory(ROCKET_SIZE, 0xffffff);
+const { throttling$, yaw$ } = playerInterface({
+  throttlingKey: 'w',
+  yawLeftKey: 'a',
+  yawRightKey: 'd',
+});
+
 gameScene.addPlayer(rocket);
+
+spaceCraftFactory({
+  throttling$,
+  gameClock$,
+  enginePower: 0.00001,
+  mass: 1,
+  rocket,
+  initialVelocity: new Vector3(0, 0, 0),
+});
+
+// gameClock$.subscribe(() => {
+//   //   const direction = rocket.up.clone().applyEuler(rocket.rotation);
+//   //   const thrust = direction.clone().multiplyScalar(0.005);
+//   //   //   rocket.position.addVectors(rocket.position, velocity);
+// });
