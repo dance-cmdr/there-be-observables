962436d359382e4c28e153cc54b297784cda3ba9
Alexandros Spyropoulos <alex.spyropoulos@dazn.com>
2019-07-26 14:42:43 +0100

    adds slides

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import {
 import { spaceCraftDestroyed, prepareSpawnSpaceCraftWithId } from './Spacecraft/SpaceCraft';
 import { ObjectLoader } from 'three';
 
+
 import rocketModel from './Graphics/Rocket/model.json';
 
 import { collitionDetection, COLLISION_TYPES } from './CollisionDetection';
